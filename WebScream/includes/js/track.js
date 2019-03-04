7fe702e48feb04c2d58956adc764b1348675d45a
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2019-03-04 17:22:20 +0100

    Implemented orderList and variables, partly.

--- WebScream/includes/js/track.js
+++ WebScream/includes/js/track.js
@@ -1,5 +1,95 @@
-﻿var S3M = (function () {
+﻿var ascii = new TextDecoder('ascii');
+
+var S3M = (function () {
 	var FLAGS = Object.freeze({ "ST2_vibrato": 1, "ST2_tempo": 2, "Amiga_slides": 4, "Zero_vol_optimisations": 8, "Amiga_limits": 16, "Enable_SB_filter_sfx": 32, "ST300_volume_slides": 64, "PtrSpecial_valid": 128 });
+
+	var _readHeader = function (s3mFile, fileData) {
+		let fileDataView = new DataView(fileData);
+
+		let header = {
+			// static header properties
+			title: ascii.decode(new Uint8Array(fileData, 0x00, 28)).split("\0")[0],
+			sig1: fileDataView.getUint8(0x1C),
+			type: fileDataView.getUint8(0x1D),
+			reserved: fileDataView.getUint16(0x1E, true), // 00 00
+
+			orderCount: fileDataView.getUint16(0x20, true),
+			instrumentCount: fileDataView.getUint16(0x22, true),
+			patternCount: fileDataView.getUint16(0x24, true),
+			flags: fileDataView.getUint16(0x26, true),
+			trackerVersion: fileDataView.getUint16(0x28, true),
+			sampleType: fileDataView.getUint16(0x2A, true),
+			sig2: ascii.decode(new Uint8Array(fileData, 0x2C, 4)), // 'SCRM'
+
+			globalVolume: fileDataView.getUint8(0x30),
+			initialSpeed: fileDataView.getUint8(0x31),
+			initialTempo: fileDataView.getUint8(0x32),
+			masterVolume: fileDataView.getUint8(0x33) & 0x7F, // 0111 1111
+			stereo: (fileDataView.getUint8(0x33) & 0x80) > 0, // 1000 0000
+			ultraClickRemoval: fileDataView.getUint8(0x34),
+			defaultPan: fileDataView.getUint8(0x35),
+			reservedData: new Uint8Array(fileData, 0x36, 8),
+			ptrSpecial: fileDataView.getUint16(0x3E, true), // parapointer
+
+			channelSettings: new Uint8Array(fileData, 0x40, 32) // 0x40 and 0x50
+		};
+
+		// calculated header properties
+		Object.assign(header, {
+			orderList: new Uint8Array(fileData, 0x60, header.orderCount)
+		});
+
+		return header;
+	};
+
+	var _readInstruments = function (s3mFile, fileData) {
+		let ptrInstruments = new DataView(fileData, 0x60 + s3mFile.header.orderCount, s3mFile.header.instrumentCount * 2); // parapointers
+
+		let instruments = [];
+		
+		for (let i = 0; i < s3mFile.header.instrumentCount; i++) {
+			let offset = ptrInstruments.getUint16(i * 2, true) * 16;
+			let instrumentHeaderView = new DataView(fileData, offset, 0x50);
+
+			let instrument = {
+				type: instrumentHeaderView.getUint8(0x00),
+				filename: ascii.decode(new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 1, 12)).split("\0")[0]
+			};
+
+			if (instrument.type === 0) {
+				// Comment-only instrument. Do nothing.
+			}
+			else if (instrument.type === 1) {
+				// PCM instrument
+				Object.assign(instrument, {
+					pcmOffset: instrumentHeaderView.getUint8(0x0D) * 0x010000 + instrumentHeaderView.getUint16(0x0E, true), // parapointer
+					length: instrumentHeaderView.getUint32(0x10, true),
+					loopStart: instrumentHeaderView.getUint32(0x14, true),
+					loopEnd: instrumentHeaderView.getUint32(0x18, true),
+					volume: instrumentHeaderView.getUint8(0x1C),
+					reserved: instrumentHeaderView.getUint8(0x1D),
+					pack: instrumentHeaderView.getUint8(0x1E),
+					flags: instrumentHeaderView.getUint8(0x1F),
+
+					c2spd: instrumentHeaderView.getUint32(0x20),
+					internal: new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 0x24, 12),
+
+					title: ascii.decode(new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 0x30, 28)).split("\0")[0],
+					sig: ascii.decode(new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 0x4C, 4)) // 'SCRS'
+				});
+
+				Object.assign(instrument, {
+					pcmData: fileData.slice(instrument.pcmOffset * 16, instrument.pcmOffset * 16 + instrument.length)
+				});
+			} else if (instrument.type === 2) {
+				console.log("FM instruments not supported.");
+			}
+
+			instruments.push(instrument);
+		}
+
+		return instruments;
+	};
 
 	var open = function (input, callback) {
 		// Check for the various File API support.
@@ -7,8 +97,6 @@
 			if (input.files.length > 0) {
 				var file = input.files[0];
 				console.log(file.name);
-
-				var ascii = new TextDecoder('ascii');
 
 				var reader = new FileReader();
 				reader.onload = function () {
@@ -27,91 +115,12 @@
 						alert('Not a valid S3M file!');
 					}
 
-					var fileDataView = new DataView(fileData);
-
 					var s3mFile = {
-						fileName: file.name,
-						_data: fileData,
-
-						header: {
-							// static header properties
-							title: ascii.decode(new Uint8Array(fileData, 0x00, 28)).split("\0")[0],
-							sig1: fileDataView.getUint8(0x1C),
-							type: fileDataView.getUint8(0x1D),
-							reserved: fileDataView.getUint16(0x1E, true), // 00 00
-
-							orderCount: fileDataView.getUint16(0x20, true),
-							instrumentCount: fileDataView.getUint16(0x22, true),
-							patternCount: fileDataView.getUint16(0x24, true),
-							flags: fileDataView.getUint16(0x26, true),
-							trackerVersion: fileDataView.getUint16(0x28, true),
-							sampleType: fileDataView.getUint16(0x2A, true),
-							sig2: ascii.decode(new Uint8Array(fileData, 0x2C, 4)), // 'SCRM'
-
-							globalVolume: fileDataView.getUint8(0x30),
-							initialSpeed: fileDataView.getUint8(0x31),
-							initialTempo: fileDataView.getUint8(0x32),
-							masterVolume: fileDataView.getUint8(0x33),
-							ultraClickRemoval: fileDataView.getUint8(0x34),
-							defaultPan: fileDataView.getUint8(0x35),
-							reservedData: new Uint8Array(fileData, 0x36, 8),
-							ptrSpecial: fileDataView.getUint16(0x3E, true), // parapointer
-
-							channelSettings: new Uint8Array(fileData, 0x40, 32)
-						},
-						instruments: [],
-						patterns: []
+						fileName: file.name
 					};
 
-					// calculated header properties
-					Object.assign(s3mFile.header, {
-						orderList: new Uint8Array(fileData, 0x60, s3mFile.header.orderCount)
-					});
-
-					var ptrInstruments = new DataView(fileData, 0x60 + s3mFile.header.orderCount, s3mFile.header.instrumentCount * 2); // parapointers
-					for (let i = 0; i < s3mFile.header.instrumentCount; i++) {
-						let offset = ptrInstruments.getUint16(i * 2, true) * 16;
-						let instrumentHeaderView = new DataView(fileData, offset, 0x50);
-
-						let instrument = {
-							type: instrumentHeaderView.getUint8(0x00),
-							filename: ascii.decode(new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 1, 12)).split("\0")[0]
-						};
-
-						// PCM instrument
-						if (instrument.type === 1) {
-							Object.assign(instrument, {
-								pcmOffset: instrumentHeaderView.getUint8(0x0D) * 0x010000 + instrumentHeaderView.getUint16(0x0E, true), // parapointer
-								length: instrumentHeaderView.getUint32(0x10, true),
-								loopStart: instrumentHeaderView.getUint32(0x14, true),
-								loopEnd: instrumentHeaderView.getUint32(0x18, true),
-								volume: instrumentHeaderView.getUint8(0x1C),
-								reserved: instrumentHeaderView.getUint8(0x1D),
-								pack: instrumentHeaderView.getUint8(0x1E),
-								flags: instrumentHeaderView.getUint8(0x1F),
-
-								c2spd: instrumentHeaderView.getUint32(0x20),
-								internal: new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 0x24, 12),
-
-								title: ascii.decode(new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 0x30, 28)).split("\0")[0],
-								sig: ascii.decode(new Uint8Array(instrumentHeaderView.buffer, instrumentHeaderView.byteOffset + 0x4C, 4)) // 'SCRS'
-							});
-
-							// TODO: Read sample data
-							let pcmData = fileData.slice(instrument.pcmOffset * 16, instrument.pcmOffset * 16 + instrument.length);
-
-							let pcmBlob = new Blob([pcmData], { type: 'audio/pcm' });
-
-							//var a = document.createElement("a"),
-							//	url = URL.createObjectURL(pcmBlob);
-							//a.href = url;
-							//a.download = i + "." + instrument.filename + ".pcm";
-							//a.innerText = i + "." + instrument.filename + ".pcm";
-							//document.body.appendChild(a);
-						}
-
-						s3mFile.instruments.push(instrument);
-					}
+					Object.assign(s3mFile, { header: _readHeader(s3mFile, fileData) });
+					Object.assign(s3mFile, { instruments: _readInstruments(s3mFile, fileData) });
 
 					// TODO: Patterns
 					//ptrPatterns: new Uint16Array(fileData, 96 + s3mFile.header.orderCount + s3mFile.header.instrumentCount * 2, s3mFile.header.patternCount)
